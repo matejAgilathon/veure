d3a071db36d6b950d57422c0b11a5fc16aa4d7c8
Matej Pavic <mpavic.dev@gmail.com>
2023-02-22 13:58:51 +0100

    initial commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img src="../assets/app-logo.png" alt="veure logo" />
+    <button @click="signInWithGoogle">Sign in with Google</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      //
+    };
+  },
+  methods: {
+    signInWithGoogle() {
+      console.log("Sign in with Google");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  padding-top: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+}
+img {
+  width: 100px;
+}
+</style>
