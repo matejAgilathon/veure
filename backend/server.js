034c4e1ea594769c8808b68e46575413916a9d0c
Matej Pavic <mpavic.dev@gmail.com>
2023-02-28 16:01:31 +0100

    refactoring backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const db = require("./models");
 const api = require("./routes/api");
 
-
-const app = express();
-
+//middleware
 app.use("/api", api)
 app.use(express.json());
 app.use(cors());
