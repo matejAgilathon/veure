1ef83a6c934c63429697418eb23877f3e2a5a976
Matej Pavic <mpavic.dev@gmail.com>
2023-03-20 11:58:44 +0100

    extracting object arguments

--- backend/controllers/connections/acceptConnection.js
+++ backend/controllers/connections/acceptConnection.js
@@ -9,18 +9,18 @@ const acceptConnection = async (req, res) => {
     if (!user || !connection) {
       return res.status(404).json({ success: false, err: "User not found" });
     }
-    const connectionUpdateRequest = await Connection.update(
-      {
-        status: "connected"
+    const update = {
+      status: "connected",
+    };
+    const where = {
+      where: {
+        userId: Number(id),
+        connectionId: Number(userId),
       },
-      {
-        where: {
-          userId: Number(id),
-          connectionId: Number(userId),
-        },
-      }
-    );
-    res.status(204).json({ success: true , connectionUpdateRequest});
+    };
+
+    const connectionUpdateRequest = await Connection.update(update, where);
+    res.status(204).json({ success: true, connectionUpdateRequest });
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, err });
